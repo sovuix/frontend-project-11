77ea32455a2b740dda00e79ce99ae5017668f618
sovuix <sovuix@gmail.com>
2025-03-15 18:02:36 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap';
+import * as yup from 'yup';
+import { formData, showError, clearError } from './view';
+
+// Создаем схему валидации для URL
+const urlSchema = yup.object().shape({
+  url: yup
+    .string()
+    .required('Поле обязательно для заполнения')
+    .url('Некорректный URL'),
+});
+
+const feeds = [];
+
+function addFeed(url) {
+  if (feeds.includes(url)) {
+    showError('Этот URL уже существует');
+    return false;
+  }
+
+  feeds.push(url);
+  clearError();
+}
+
+const form = document.querySelector('.rss-form');
+const urlInput = form.querySelector('#url-input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const url = urlInput.value.trim(); 
+
+  urlSchema
+    .validate({ url })
+    .then(() => {
+      if (addFeed(url)) {
+        form.reset();
+        urlInput.focus();
+      }
+    })
+    .catch((err) => {
+      if (err.errors[0] === 'Поле обязательно для заполнения') {
+        showError('Поле обязательно для заполнения');
+      } else if (err.errors[0] === 'Некорректный URL') {
+        showError('Некорректный URL');
+      }
+    });
+});
