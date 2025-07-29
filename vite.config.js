6bfbbe7a89aaa5938b12db56ad25185d194de153
sovuix <sovuix@gmail.com>
2025-07-29 22:55:23 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
+// /** @type {import('vite').UserConfig} */
+// export default {
+//     server: {
+//         cors: true,
+//     }
+// }
+
 /** @type {import('vite').UserConfig} */
+import path from 'path';
+
 export default {
-    server: {
-        cors: true,
-    }
-}
+  server: {
+    cors: true,
+  },
+  resolve: {
+    alias: {
+      'bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  build: {
+    assetsInclude: ['**/*.css', '**/*.js'],
+  },
+};
