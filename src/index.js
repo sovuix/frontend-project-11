1e95aa3f8cee5f2f6c9d621c6149b4585b6bf298
sovuix <sovuix@gmail.com>
2025-08-05 07:25:06 +0300

    draft

--- src/index.js
+++ src/index.js
@@ -3,24 +3,23 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import * as yup from 'yup';
-import { setLocale } from 'yup';
-import initI18n from './i18n/i18n.js'
+import * as yup from "yup";
+import { setLocale } from "yup";
+import initI18n from "./i18n/i18n.js";
 import { watch } from "./view.js";
 
-import getUrl from './rssFeedParcer.js';
-
+import getUrl from "./rssFeedParcer.js";
+import _ from "lodash";
 
 const elements = {
-  urlInput: document.querySelector('#url-input'),
-  form: document.querySelector('.rss-form'),
-  feedback: document.querySelector('.feedback'),
-  posts: document.querySelector('.posts'),
-  feeds: document.querySelector('.feeds'),
-  templatePosts: document.getElementById('posts'),
-  templateFeeds: document.getElementById('feeds'),
+  urlInput: document.querySelector("#url-input"),
+  form: document.querySelector(".rss-form"),
+  feedback: document.querySelector(".feedback"),
+  posts: document.querySelector(".posts"),
+  feeds: document.querySelector(".feeds"),
+  templatePosts: document.getElementById("posts"),
+  templateFeeds: document.getElementById("feeds"),
 };
-
 
 const runApp = () => {
   const state = {
@@ -28,42 +27,75 @@ const runApp = () => {
     errors: [],
     posts: [],
     loading: {
-      status: 'idle',
+      status: "idle",
       // error: '',
     },
     form: {
-      status: 'filling', // 
-      // error: '' // 
-    }
-  }
+      status: "filling", //
+      // error: '' //
+    },
+  };
 
-  initI18n()
-    .then((i18n) => {
-      setLocale({
-        mixed: {
-          required: 'errors.required',
-          notOneOf: 'errors.duplicateUrl',
-        },
-        string: {
-          url: 'errors.invalidUrl',
-        },
-      });
+  initI18n().then((i18n) => {
+    setLocale({
+      mixed: {
+        required: "errors.required",
+        notOneOf: "errors.duplicateUrl",
+      },
+      string: {
+        url: "errors.invalidUrl",
+      },
+    });
 
-      const watchedState = watch(
-        state,
-        i18n,
-        elements
+    const watchedState = watch(state, i18n, elements);
+
+    if (elements.form) {
+      elements.form.addEventListener("submit", (e) =>
+        handleFormSubmit(e, watchedState)
       );
+    }
+  });
 
-      if (elements.form) {
-        elements.form.addEventListener('submit', (e) => handleFormSubmit(e, watchedState))
+  // const updateFeeds = (watchedState) => {
+  //   watchedState.feeds.forEach((feed) => {
+  //     getUrl(feed.url)
+  //       .then((response) => {
+  //         const newPosts = _.differenceBy(
+  //           response.posts,
+  //           watchedState.posts,
+  //           "link"
+  //         );
+
+  //         if (newPosts.length > 0) {
+  //           watchedState.posts.unshift(...newPosts);
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error(error.message);
+  //       });
+  //   });
+  // };
+
+  const updateFeeds = (watchedState) => {
+    const feedPromises = watchedState.feeds.map((feed) =>
+      getUrl(feed.url).then((response) => {
+        return _.differenceBy(response.posts, watchedState.posts, "link");
+      })
+    );
+
+    Promise.all(feedPromises).then((allNewPosts) => {
+      const newPosts = _.flatten(allNewPosts);
+      if (newPosts.length > 0) {
+        watchedState.posts.unshift(...combinedNewPosts);
       }
-    })
+      setTimeout(() => updateFeeds(watchedState), 5000);
+    });
+  };
 
   function handleFormSubmit(e, watchedState) {
     e.preventDefault();
-    watchedState.form.status = 'submitted';
-    watchedState.loading.status = 'idle';
+    watchedState.form.status = "submitted";
+    watchedState.loading.status = "idle";
     const url = elements.urlInput.value.trim();
 
     const schema = yup.object().shape({
@@ -71,67 +103,41 @@ const runApp = () => {
         .string()
         .required()
         .url()
-        .notOneOf(watchedState.feeds.map(feed => feed.url))
+        .notOneOf(watchedState.feeds.map((feed) => feed.url)),
     });
 
     schema
       .validate({ url })
       .then(() => {
-
-        // watchedState.feeds = [...watchedState.feeds, url];
         watchedState.errors = [];
 
-        watchedState.loading.status = 'loading';
+        watchedState.loading.status = "loading";
         console.log(state.loading.status);
-        
+
         getUrl(url)
           .then((response) => {
-            watchedState.loading.status = 'loaded';
+            watchedState.loading.status = "loaded";
             watchedState.errors = [];
             console.log(state.loading.status);
 
-
-            // проверка
             watchedState.feeds.push(response.feed);
-            // тоже надо делать push
-            // watchedState.posts = response.posts;
-            watchedState.posts.push(...response.posts)
-            // watchedState.loading.status = false;
-            // console.log(watchedState.posts[0]);
-            // console.log(watchedState.feeds[0]);
-            watchedState.feeds.map(feed => console.log(feed.url))
+            watchedState.posts.push(...response.posts);
+
+            // setTimeout(() => updateFeeds(watchedState), 5000);
           })
           .catch((error) => {
             watchedState.errors = [error.message];
-            // watchedState.loading.error = error.message;
-            // watchedState.loading.status = false;
+          })
+          .finally(() => {
+            updateFeeds(watchedState);
           });
 
-        // console.log(watchedState.feeds);
-
-
-        elements.urlInput.value = '';
+        elements.urlInput.value = "";
       })
       .catch((error) => {
         watchedState.errors = error.errors;
       });
   }
+};
 
-
-}
 runApp();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
