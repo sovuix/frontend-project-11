5541af1ca44ff20bcc64f86237467a318d161bc3
sovuix <sovuix@gmail.com>
2025-08-30 09:56:11 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// /** @type {import('vite').UserConfig} */
-// export default {
-//     server: {
-//         cors: true,
-//     }
-// }
-
 /** @type {import('vite').UserConfig} */
+
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   server: {
