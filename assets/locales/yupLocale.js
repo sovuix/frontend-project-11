12bfcde269b0e8d1052de22d0716ef70c0ad82a6
sovuix <sovuix@gmail.com>
2025-09-04 21:41:18 +0300

    moving the configuration to a separate module

---
+++ assets/locales/yupLocale.js
@@ -0,0 +1,11 @@
+const yupLocale = {
+  mixed: {
+    required: 'errors.required',
+    notOneOf: 'errors.duplicateUrl',
+  },
+  string: {
+    url: 'errors.invalidUrl',
+  },
+}
+
+export default yupLocale
